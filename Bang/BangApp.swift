881b389b1db454811e17e7187ac505200595cd19
Harem Hasan <harem2024@gmail.com>
2024-08-19 19:35:37 +0300

    Initial Commit

---
+++ Bang/BangApp.swift
@@ -0,0 +1,17 @@
+//
+//  BangApp.swift
+//  Bang
+//
+//  Created by Harem on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
