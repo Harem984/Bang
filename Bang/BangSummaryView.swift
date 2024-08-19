743c184638aa924167bc90a46911de472842ff20
Harem Hasan <harem2024@gmail.com>
2024-08-19 20:39:46 +0300

    19/8/2024

---
+++ Bang/BangSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  BangSummaryView.swift
+//  Bang
+//
+//  Created by Harem on 19/08/2024.
+//
+
+import SwiftUI
+
+struct BangSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BangSummaryView()
+}
