650e731d6e8c93d620534b5878ee9202202b443e
Harem Hasan <harem2024@gmail.com>
2024-08-20 00:28:26 +0300

    Add BackgroundView

---
+++ Bang/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  Bang
+//
+//  Created by Mac on 20/08/2024.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
